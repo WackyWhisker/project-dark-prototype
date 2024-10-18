73ebeff0149b3c6a431ef8ed00f04a87ebab81c8
Christian Reichel <christian.herrmann.sv@gmail.com>
2024-10-18 21:22:36 +0200

    fleshed out the level a little bit with metrics and small traversal obstacles

--- Source/Dark/Private/StateMachine/States/DkPlayerStateAttack.cpp
+++ Source/Dark/Private/StateMachine/States/DkPlayerStateAttack.cpp
@@ -24,6 +24,7 @@ void UDkPlayerStateAttack::OnStateEnter(AActor* StateOwner)
 		AnimInstance->Montage_Play(AttackMontage);
 	}
 	UE_LOG(LogTemp, Warning, TEXT("Attack Index: %s"), *FString::FromInt(AttackIndex));
+	AttackIndex++;
 	if (PlayerRef->GetCharacterMovement()->Velocity.Length() == 0.0f && PlayerRef->GetCharacterMovement()->IsMovingOnGround())
 	{
 		PlayerRef->StateManager->SwitchStateByKey("Idle");
