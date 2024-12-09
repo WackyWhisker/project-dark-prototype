ed21c63bdff1383bbfef914d0aa7c57b2ad2b53b
Christian Reichel <christian.herrmann.sv@gmail.com>
2024-12-09 19:30:18 +0100

    fixed couple of new materials and textures to be compatible with the retro graphics; started on castle arrenger subsystem

---
+++ Source/Dark/Private/Subsystem/CastleSubsystem.cpp
@@ -0,0 +1,10 @@
+﻿// Copyright @ Christian Reichel
+
+#include "Subsystem/CastleSubsystem.h"
+
+DEFINE_LOG_CATEGORY(CastleLog)
+
+void UCastleSubsystem::TestSubsystemAccess()
+{
+	UE_LOG(CastleLog, Warning, TEXT("Castle Subsystem accessed successfully"));
+}
---
+++ Source/Dark/Public/Subsystem/CastleSubsystem.h
@@ -0,0 +1,37 @@
+﻿// Copyright @ Christian Reichel
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CastleSubsystem.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(CastleLog, Log, All);
+
+UCLASS()
+class DARK_API UCastleSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	//Public Methods -------------------
+	
+	// Simple function we can call to verify subsystem is accessible
+	UFUNCTION(BlueprintCallable, Category = "Castle")
+	void TestSubsystemAccess();
+
+public:
+	//Public Properties ---------------
+
+protected:
+	//Protected Methods ---------------
+
+protected:
+	//Protected Properties ------------
+
+private:
+	//Private Methods ----------------
+
+private:
+	//Private Properties -------------
+};
