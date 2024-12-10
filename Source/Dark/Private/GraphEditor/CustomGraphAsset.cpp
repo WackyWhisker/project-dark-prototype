e19da00cc8b98b5647a3f13eef3c2d76c2b77c0e
Christian Reichel <christian.herrmann.sv@gmail.com>
2024-12-10 20:46:31 +0100

    added custom graph asset that acts as a container for new graphs

---
+++ Source/Dark/Private/GraphEditor/CustomGraphAsset.cpp
@@ -0,0 +1,9 @@
+﻿// Copyright @ Christian Reichel
+
+
+#include "GraphEditor/CustomGraphAsset.h"
+
+UCustomGraphAsset::UCustomGraphAsset()
+{
+	EdGraph = nullptr;
+}
---
+++ Source/Dark/Public/GraphEditor/CustomGraphAsset.h
@@ -0,0 +1,22 @@
+﻿// Copyright @ Christian Reichel
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "CustomGraphAsset.generated.h"
+
+class UEdGraph;
+
+UCLASS()
+class DARK_API UCustomGraphAsset : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UCustomGraphAsset();
+
+	// The actual graph that contains nodes and connections
+	UPROPERTY()
+	UEdGraph* EdGraph;
+};
