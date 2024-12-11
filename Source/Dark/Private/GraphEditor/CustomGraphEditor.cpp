27ff913bf2a73e97da730a4be309c596599ab503
Christian Reichel <christian.herrmann.sv@gmail.com>
2024-12-11 11:35:42 +0100

    created the Graph Editor widget, which will be our actual visual interface for editing the graph.

---
+++ Source/Dark/Private/GraphEditor/CustomGraphEditor.cpp
@@ -0,0 +1,32 @@
+﻿// Copyright @ Christian Reichel
+
+
+#include "GraphEditor/CustomGraphEditor.h"
+#include "GraphEditor/CustomGraphAsset.h"
+#include "GraphEditorActions.h"
+#include "Framework/Commands/GenericCommands.h"
+
+void SCustomGraphEditor::Construct(const FArguments& InArgs, UCustomGraphAsset* InGraphAsset)
+{
+	GraphAsset = InGraphAsset;
+
+	// Initialize command list
+	GraphEditorCommands = MakeShareable(new FUICommandList);
+
+	// Create the appearance info for the graph editor
+	FGraphAppearanceInfo AppearanceInfo;
+	AppearanceInfo.CornerText = FText::FromString("Custom Graph");
+
+	// Create the graph editor widget
+	GraphEditorWidget = SNew(SGraphEditor)
+		.AdditionalCommands(GraphEditorCommands)
+		.IsEditable(true)
+		.Appearance(AppearanceInfo)
+		.GraphToEdit(GraphAsset->EdGraph);
+
+	// Set this widget to be the graph editor
+	ChildSlot
+	[
+		GraphEditorWidget.ToSharedRef()
+	];
+}
---
+++ Source/Dark/Public/GraphEditor/CustomGraphEditor.h
@@ -0,0 +1,29 @@
+﻿// Copyright @ Christian Reichel
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GraphEditor.h"
+#include "Widgets/SCompoundWidget.h"
+
+class UCustomGraphAsset;
+
+class SCustomGraphEditor : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SCustomGraphEditor) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, UCustomGraphAsset* InGraphAsset);
+
+private:
+	// Pointer to the graph asset being edited
+	TWeakObjectPtr<UCustomGraphAsset> GraphAsset;
+
+	// The actual graph editor widget
+	TSharedPtr<SGraphEditor> GraphEditorWidget;
+
+	// Commands handled by this editor
+	TSharedPtr<FUICommandList> GraphEditorCommands;
+};
