e10c33abf289ed5c64dae1d5361b5bf5b362c764
Christian Reichel <christian.herrmann.sv@gmail.com>
2025-01-27 09:36:19 +0100

    disabled interaction component debugs; reverted to default PC material; started testmap for maya pipeline rnd

--- Source/Dark/Private/Component/DkInteractionComponent.cpp
+++ Source/Dark/Private/Component/DkInteractionComponent.cpp
@@ -31,15 +31,15 @@ void UDkInteractionComponent::BeginPlay()
 	if (!PlayerRef)
 	{
 		PlayerRef = Cast<ADkCharacter>(GetOwner());
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, 
-			FString::Printf(TEXT("PlayerRef Set: %s"), PlayerRef ? TEXT("Yes") : TEXT("No")));
+		// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, 
+		// 	FString::Printf(TEXT("PlayerRef Set: %s"), PlayerRef ? TEXT("Yes") : TEXT("No")));
 	}
 
 	if (PlayerRef)
 	{
 		PlayerCameraRef = PlayerRef->FindComponentByClass<UCameraComponent>();
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, 
-			FString::Printf(TEXT("CameraRef Set: %s"), PlayerCameraRef ? TEXT("Yes") : TEXT("No")));
+		// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, 
+		// 	FString::Printf(TEXT("CameraRef Set: %s"), PlayerCameraRef ? TEXT("Yes") : TEXT("No")));
 
 		// Manually attach the sphere
 		if (SphereCollision && PlayerRef->GetRootComponent())
@@ -50,8 +50,8 @@ void UDkInteractionComponent::BeginPlay()
 			);
             
 			// Debug message for sphere
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, 
-				FString::Printf(TEXT("Sphere Radius: %f"), InteractionRadius));
+			// GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, 
+			// 	FString::Printf(TEXT("Sphere Radius: %f"), InteractionRadius));
 		}
 	}
 }
@@ -60,7 +60,7 @@ void UDkInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Debug draw the sphere
-	if (PlayerRef)
+	/*if (PlayerRef)
 	{
 		DrawDebugSphere(
 			GetWorld(),
@@ -73,7 +73,7 @@ void UDkInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType
 			0,
 			1
 		);
-	}
+	}*/
 
 	UpdateClosestInteractable();
 }
@@ -82,16 +82,16 @@ void UDkInteractionComponent::UpdateClosestInteractable()
 {
 	if (!PlayerCameraRef || !PlayerRef)
 	{
-		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("Missing PlayerCameraRef or PlayerRef!"));
-		return;
+		// GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Red, TEXT("Missing PlayerCameraRef or PlayerRef!"));
+		// return;
 	}
 
 	TArray<AActor*> OverlappingActors;
 	SphereCollision->GetOverlappingActors(OverlappingActors);
     
 	// Debug overlap count
-	GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, 
-		FString::Printf(TEXT("Overlapping Actors: %d"), OverlappingActors.Num()));
+	// GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Yellow, 
+	// 	FString::Printf(TEXT("Overlapping Actors: %d"), OverlappingActors.Num()));
 
 	// First filter for interface implementation
 	TArray<AActor*> InteractableActors;
@@ -107,22 +107,22 @@ void UDkInteractionComponent::UpdateClosestInteractable()
 	}
 
 	// Debug interactable count
-	GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, 
-		FString::Printf(TEXT("Interactable Actors: %d"), InteractableActors.Num()));
+	// GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Blue, 
+	// 	FString::Printf(TEXT("Interactable Actors: %d"), InteractableActors.Num()));
 
 	TArray<AActor*> ValidTargets;
 	if (FilterInteractablesByViewCone(InteractableActors, ValidTargets))
 	{
 		FocusedActor = ValidTargets[0];
         
-		// Debug message for currently focused interactable
-		GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Green, 
-			FString::Printf(TEXT("Speak to %s"), *FocusedActor->GetName()));
+		// // Debug message for currently focused interactable
+		// GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Green, 
+		// 	FString::Printf(TEXT("Speak to %s"), *FocusedActor->GetName()));
 	}
 	else
 	{
 		FocusedActor = nullptr;
-		GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Orange, TEXT("No valid target in view cone"));
+		// GEngine->AddOnScreenDebugMessage(-1, 0.0f, FColor::Orange, TEXT("No valid target in view cone"));
 	}
 }
 
